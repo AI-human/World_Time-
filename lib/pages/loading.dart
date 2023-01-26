8d72602e04cd4d10ea54a55ea1915eade8204704
Kashfi <tahmidulkashfi15@gmail.com>
2023-01-26 14:37:42 +0600

    packages & using api

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -1,4 +1,7 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class Loading extends StatefulWidget {
   const Loading({super.key});
@@ -8,6 +11,23 @@ class Loading extends StatefulWidget {
 }
 
 class _LoadingState extends State<Loading> {
+  void getData() async {
+    var response = await http
+        .get(Uri.parse('https://jsonplaceholder.typicode.com/todos/1'));
+    Map data = jsonDecode(response.body); // for converting json file into Map
+    print(data);
+    print(data['title']);
+  }
+
+  int cnt = 0;
+  @override
+  void initState() {
+    super.initState();
+    print('initState function ran'); // it
+    getData();
+    print('dosen\'t wait for get data or block this line');
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
