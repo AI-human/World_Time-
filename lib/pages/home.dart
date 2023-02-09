c2e15890c9f45bd8ad0065637fcfb005a75208fe
Kashfi <tahmidulkashfi15@gmail.com>
2023-02-09 14:03:30 +0600

    Passing Route Data

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -4,19 +4,25 @@ class Home extends StatefulWidget {
   // const({super.key});
 
   @override
-  State<Home> createState() => HhomeState();
+  State<Home> createState() => HomeState();
 }
 
-class HhomeState extends State<Home> {
+class HomeState extends State<Home> {
+  Map data = {};
+
   @override
   Widget build(BuildContext context) {
+    data =
+        ModalRoute.of(context)!.settings.arguments as Map; // passing route data
+    print(data);
+
     return Scaffold(
       body: SafeArea(
         child: Column(children: <Widget>[
           TextButton.icon(
             onPressed: () {
-              Navigator.pushNamed(context,
-                  '/location'); // here how pressed and push to location page and if we want to go back to home its gonna pop like stack
+              Navigator.pushNamed(context, '/location');
+              // here how pressed and push to location page and if we want to go back to home its gonna pop like stack
             },
             icon: Icon(Icons.edit_location),
             label: Text('Edit Location'),
